269f9ec1c58c39d7981a740919cca2e378d28428
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-02-16 18:24:23 -0500

    Add files via upload

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby" />
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
+
+/* On 2/16 you left off at https://www.gatsbyjs.com/docs/tutorial/part-one/
+on the section "What Are Props?" in the tutorial. */
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,11 @@ import React from "react"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+    <div style={{ color: `orange` }}>
       <h1>Hello Gatsby!</h1>
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <p>This is me testing out the Gatsby today.</p>
     </div>
   )
 }
